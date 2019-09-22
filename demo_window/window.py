eb6b77c5818e66b14b5a51f14b58f86dd30e5051
fred-och-karlek <yiyuw@student.chalmers.se>
2019-09-22 15:48:28 +0200

    demo window

--- demo_window/window.py
+++ demo_window/window.py
@@ -1,11 +1,35 @@
-import wx
-
-class MyFrame(wx.Frame):
+import subprocess
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+import sys
+class WindowDemo(QWidget):
     def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        self.Show()
+        super(WindowDemo, self).__init__()
+        btn1 = QPushButton(u'run camera', self)
+        btn2 = QPushButton(u'stop running', self)
 
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
+        btn1.clicked.connect(self.handle_event)
+        btn2.clicked.connect(self.handle_event)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(btn1)
+        hbox.addWidget(btn2)
+        hbox.addStretch(0)
+        self.setLayout(hbox)
+        self.setWindowTitle("have a nice day!")
+
+    def handle_event(self):
+
+        sender = self.sender()
+        clickevent = sender.text()
+        if clickevent == u'run camera':
+            p1 = subprocess.Popen('ls -l', shell=True)
+
+        else:
+            p2 = subprocess.Popen('ls -a', shell=True)
+            print('hello')
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = WindowDemo()
+    win.show()
+    sys.exit(app.exec_())
